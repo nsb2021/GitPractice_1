8dd6beca059593780d7859aa52d02857da2509ec
Nasiba <6475650@gmail.com>
2021-04-24 23:24:26 -0700

    added more work after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
 
         System.out.println("Hello world");
         System.out.println("Adding GitHub Now");
+        System.out.println("YAY STUFFS ARE ON GITHUB");
+
     }
 }
